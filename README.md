5c28f354b836133df34ec7b98a1d77eb0fe91794
Arto <aruthart@gmail.com>
2021-01-07 17:57:42 +0200

    MemStats callback

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 > Utilities for cuda memory usage inspection for pytorch and fastai.
 
 
-This file will become your README and also the index of your documentation.
-
 ## Install
 
 `pip install -qq git+git://github.com/arampacha/pt_memprofile.git`
--- pt_memprofile/_nbdev.py
+++ pt_memprofile/_nbdev.py
@@ -3,9 +3,12 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"MemHooks": "00_core.ipynb",
+         "plot_log": "00_core.ipynb",
+         "reset_grad": "00_core.ipynb",
          "profile_memory": "00_core.ipynb",
          "MemProfileCallback": "00_core.ipynb",
-         "Learner.profile_memory": "00_core.ipynb"}
+         "Learner.profile_memory": "00_core.ipynb",
+         "MemStatsCallback": "00_core.ipynb"}
 
 modules = ["core.py"]
 
